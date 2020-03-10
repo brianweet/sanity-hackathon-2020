8cf8c1f950beb479b3c5ea837889f05ce092bca7
Brian Weeteling <brianweeteling@gmail.com>
2020-03-10 22:28:55 +0100

    Try Sanity.Linq and GraphQL (not really) on backend

---
+++ sanity-dotnet/Controllers/ProductsController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraphQL;
+using GraphQL.NewtonsoftJson;
+using GraphQL.Types;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Sanity.Linq;
+using Sanity.Linq.CommonTypes;
+
+namespace sanity_dotnet.Controllers
+{
+    // Example
+    public class Product : SanityDocument
+    {
+        public Product() : base() { }
+
+        public string Title { get; set; }
+
+        // etc...
+    }
+
+    public class ProductGraphQL
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+    }
+
+    public class Query
+    {
+        [GraphQLMetadata("Product")]
+        public ProductGraphQL GetProduct()
+        {
+            return new ProductGraphQL { Title = "R2-D2" };
+        }
+    }
+
+
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<ProductsController> _logger;
+        private readonly SanityDataContext _context;
+
+        public ProductsController(
+            ILogger<ProductsController> logger,
+            SanityDataContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<string>> Get()
+        {
+            // Returns a list of strings ordered by publish date
+            var postTitles = await _context.DocumentSet<Product>()
+                                   .OrderByDescending(p => p.Title)
+                                   .ToListAsync();
+            return postTitles.Select(x => x.Title);
+        }
+
+        [HttpGet("GetGraphQL")]
+        public async Task<string> GetGraphQL()
+        {
+            var schema = Schema.For(@"
+      type Query {
+        hello: String
+      }
+    ");
+
+            var json = await schema.ExecuteAsync(_ =>
+            {
+                _.Query = "{ hello }";
+                _.Root = new { Hello = "Hello World!" };
+            });
+
+            return json;
+        }
+    }
+
+}
---
+++ sanity-dotnet/Startup.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Sanity.Linq;
+
+namespace sanity_dotnet
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var options = new SanityOptions
+            {
+                ProjectId = "oouyzg8g",
+                Dataset = "production",
+                Token = "",
+                UseCdn = false
+            };
+
+            var sanity = new SanityDataContext(options);
+            services.AddSingleton(sanity);
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
